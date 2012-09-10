2c8009d3182016dcfaf51815b284e533f22ab287
shaik rafi <rafimca06@gmail.com>
2012-09-10 11:15:22 +0530

    Update te.py

--- te.py
+++ te.py
@@ -1,8 +1,10 @@
 def funct():
-    a=10
-    b=20
-    return a+b
+    a = 10
+    b = 20
+    return a + b
+
+
 def sub():
-    a=10
-    b=20
-    return b-a-1
+    a = 10
+    b = 20
+    return b - a - 1
